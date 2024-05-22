7fd688900a8f0cbcda33d7c4df8a8cce133282b3
Данила <danilapanasenko0@mail.ru>
2024-05-23 00:03:43 +0300

    add 1

--- PortalMMO/PortalMMO/urls.py
+++ PortalMMO/PortalMMO/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from ckeditor_uploader.views import upload, browse
+from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include('myapp.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
+    re_path(r'^upload/', login_required(upload), name='ckeditor_upload'),
+    re_path(r'^browse/', login_required(never_cache(browse)), name='ckeditor_browse'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- PortalMMO/myapp/views.py
+++ PortalMMO/myapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Advertisement
+
+
+class AdvertisementList(ListView):
+    model = Advertisement
+    ordering = '-some_datatime'
+    template_name = 'flatpages/Advertisement.html'
+    context_object_name = 'advertisement'
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
